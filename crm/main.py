f71f57ce5e0e14dc0ffb5435416a0d9ba18bd92b
anselmlys <anselm.lisa@gmail.com>
2026-04-30 11:24:17 +0200

    configure Sentry to capture unhandled exceptions

--- crm/main.py
+++ crm/main.py
@@ -1,7 +1,11 @@
+import sentry_sdk
+import click
+
+from crm.sentry_config import init_sentry
 from crm.db import engine, Session
 from crm.models.base import Base
-
 from crm.cli.app import cli
+
 from crm.models.user import User
 from crm.models.client import Client
 from crm.models.contract import Contract
@@ -15,5 +19,13 @@ def init_db():
 
 def main():
     '''Initialize the app and prepare the main execution flow.'''
+    init_sentry()
     init_db()
-    cli()
+
+    try:
+        cli()
+
+    # All unhandled errors will be sent to Sentry
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        click.secho('Unexpected error has occurred.', fg='red')
---
+++ crm/sentry_config.py
@@ -0,0 +1,20 @@
+import os
+import sentry_sdk
+
+from dotenv import load_dotenv
+
+
+def init_sentry():
+    '''Initialize Sentry if a DSN is configured.'''
+    load_dotenv()
+
+    sentry_dsn = os.getenv('SENTRY_DSN')
+    app_env = os.getenv('APP_ENV', 'development')
+
+    if not sentry_dsn:
+        return
+    
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        environment=app_env,
+    )
